ee183211d2c0c7d752f0e56d280892e0bb4f7043
Ronny Yabar <ronny@kde.org>
2019-06-26 00:01:53 -0500

    Set up CSS styles to screenshot generator page.

--- screenshot_generator/settings.py
+++ screenshot_generator/settings.py
@@ -122,3 +122,8 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 MEDIA_URL = '/media/'
+
+STATICFILES_DIRS = (
+    ('', os.path.join(BASE_DIR, 'static')),
+)
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
