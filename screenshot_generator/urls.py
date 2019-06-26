f405be50774d006a1f0eab6027aa706fd8b8e3c1
Ronny Yabar <ronny@kde.org>
2019-06-25 22:34:27 -0500

    Upgrade project to django 2.2

--- screenshot_generator/urls.py
+++ screenshot_generator/urls.py
@@ -1,26 +1,23 @@
 """screenshot_generator URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.10/topics/http/urls/
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
     1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
-    1. Import the include() function: from django.conf.urls import url, include
-    2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url, include
+from django.urls import include, path
 from django.conf import settings
+from django.conf.urls.static import static
 from django.views.static import serve
-from app import urls as app_urls
 
 urlpatterns = [
-    url(r'^', include(app_urls)),
-    url(r'^media/(?P<path>.*)$',
-        serve, {'document_root': settings.MEDIA_ROOT,}
-    ),
-]
+    path('', include('app.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
